0b425808a0347e5c3a9afc3a7b9c2d2ab60772eb
V-nyang <whitepony1214@gmail.com>
2024-05-12 18:46:34 +0900

    Fix: エラー時にフォーカスを合わせる

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -5,14 +5,17 @@
 
   let resultText: string = 'Please enter your name below 👇'
   let name: string
+  let nameInput: HTMLInputElement
 
   async function greet() {
     try {
       resultText = await Greet(name)
-      name = ''
     } catch (e) {
-      ShowErrorMessage(e as string)
+      await ShowErrorMessage(e as string)
+      nameInput.focus()
     }
+
+    name = ''
   }
 </script>
 
@@ -22,7 +25,9 @@
   <P class="my-8">{resultText}</P>
 
   <form on:submit|preventDefault={greet} class="flex space-x-4">
-    <Input type="text" bind:value={name} placeholder="Your name" />
+    <Input let:props placeholder="Your name">
+      <input type="text" {...props} bind:value={name} bind:this={nameInput} />
+    </Input>
     <Button on:click={greet}>Greet</Button>
   </form>
 </CenteredContainer>
