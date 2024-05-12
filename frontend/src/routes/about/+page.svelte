5d94d2b9ef104c91dea078fb34d0444801a86f2a
V-nyang <whitepony1214@gmail.com>
2024-05-12 19:18:44 +0900

    Fix: 外部サイトをブラウザで開く

--- frontend/src/routes/about/+page.svelte
+++ frontend/src/routes/about/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import Container from '$lib/layout/Container.svelte'
   import { SITE_TITLE, pageTitle } from '$lib/layout/stores'
+  import { BrowserOpenURL } from '$lib/wailsjs/runtime/runtime'
   import { A, Heading, P } from 'flowbite-svelte'
 
   $pageTitle = 'About'
@@ -9,6 +10,8 @@
 <Container>
   <Heading tag="h1">Welcome to {SITE_TITLE}</Heading>
   <P class="mt-5 ml-1">
-    Visit <A href="https://kit.svelte.dev">kit.svelte.dev</A> to read the documentation
+    Visit
+    <A on:click={() => BrowserOpenURL('https://kit.svelte.dev')}>kit.svelte.dev</A>
+    to read the documentation of SvelteKit
   </P>
 </Container>
