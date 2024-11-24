645438168f355d5d6ccf3c2c3ebf219bb58f38e0
V-nyang <l64906947+nftug@users.noreply.github.com>
2024-11-24 19:30:48 +0900

    Add: アイテムが存在しない場合のリスト表示

--- frontend/src/components/TodoList.tsx
+++ frontend/src/components/TodoList.tsx
@@ -1,4 +1,4 @@
-import { List, ListItem, SxProps, Theme } from '@mui/material'
+import { Box, List, ListItem, SxProps, Theme, Typography } from '@mui/material'
 import { useEffect } from 'react'
 import useTodoAtoms from '../atoms/todo-atoms'
 import TodoItem from './TodoItem'
@@ -16,11 +16,21 @@ const TodoList: React.FC<Props> = ({ sx }) => {
 
   return (
     <List sx={sx}>
-      {todoList.map((item) => (
-        <ListItem key={item.id}>
-          <TodoItem item={item} />
-        </ListItem>
-      ))}
+      {todoList.length === 0 ? (
+        <Box
+          sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%' }}
+        >
+          <Typography variant="h6" color="textSecondary">
+            TODOがありません。
+          </Typography>
+        </Box>
+      ) : (
+        todoList.map((item) => (
+          <ListItem key={item.id}>
+            <TodoItem item={item} />
+          </ListItem>
+        ))
+      )}
     </List>
   )
 }
